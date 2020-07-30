0bac9e45ad4d924227a1197e6ec664e4c2789fdb
lmarqs <lucas.marques@ebanx.com>
2020-07-29 21:24:04 -0300

    rename vars

--- daily-interview-pro/longest-substring-without-repeating-characters/solution.py
+++ daily-interview-pro/longest-substring-without-repeating-characters/solution.py
@@ -1,19 +1,19 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s):
+    def lengthOfLongestSubstring(self, text):
         last_seen = {}
         ans = 0
         head = 0
 
-        for i in range(len(s)):
-            c = s[i]
+        for tail in range(len(text)):
+            char = text[tail]
 
-            if c in last_seen:
-              while s[head] != c:
-                del last_seen[s[head]]
+            if char in last_seen:
+              while text[head] != char:
+                del last_seen[text[head]]
                 head += 1
               head += 1
 
-            last_seen[c] = i
-            ans = max(ans, i - head + 1)
+            last_seen[char] = tail
+            ans = max(ans, tail - head + 1)
 
         return ans
