9a596f2b876c148b34b61a6e179eb2b56384c713
lmarqs <lucas.marques@ebanx.com>
2020-07-29 21:37:15 -0300

    refact solution

--- daily-interview-pro/longest-substring-without-repeating-characters/solution.py
+++ daily-interview-pro/longest-substring-without-repeating-characters/solution.py
@@ -1,19 +1,16 @@
 class Solution:
     def lengthOfLongestSubstring(self, text):
         last_seen = {}
-        ans = 0
-        head = 0
+        result = 0
+        tail = -1
 
-        for tail in range(len(text)):
-            char = text[tail]
+        for head in range(len(text)):
+            char = text[head]
 
             if char in last_seen:
-              while text[head] != char:
-                del last_seen[text[head]]
-                head += 1
-              head += 1
+              tail = max(tail, last_seen[char])
 
-            last_seen[char] = tail
-            ans = max(ans, tail - head + 1)
+            last_seen[char] = head
+            result = max(result, head - tail)
 
-        return ans
+        return result
