03df3cfc491206f506553f7afd2deabb524bc219
lmarqs <lucas.paula@wipro.com>
2020-10-12 14:36:14 -0300

    add 'First Missing Positive Integer'

---
+++ daily-interview-pro/first-missing-positive-integer/solution.py
@@ -0,0 +1,16 @@
+def first_missing_positive(nums):
+    if not nums:
+        return 1
+
+    i = 0
+    while i < len(nums):
+      j = i
+      while nums[j] in range(len(nums)) and nums[j] != j:
+          nums[nums[j]], nums[j] = nums[j], nums[nums[j]]
+      i += 1
+
+    for i, num in enumerate(nums[1:], 1):
+        if num != i:
+            return i
+
+    return len(nums)
---
+++ daily-interview-pro/first-missing-positive-integer/test.py
@@ -0,0 +1,15 @@
+import unittest
+from solution import first_missing_positive
+
+
+class Test(unittest.TestCase):
+
+    def test_first_missing_positive(self):
+        self.assertEqual(2, first_missing_positive([3, 4, -1, 1]))
+        self.assertEqual(6, first_missing_positive([3, 4, -1, 1, 5, 2, 8]))
+        self.assertEqual(1, first_missing_positive([]))
+        self.assertEqual(1, first_missing_positive([0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
