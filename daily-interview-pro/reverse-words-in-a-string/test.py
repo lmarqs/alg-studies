99d3364e7195ff000ee1b9bd9c1d065a9ef26352
lmarqs <lucas.paula@wipro.com>
2020-10-12 17:20:18 -0300

    add 'Reverse Words in a String'

---
+++ daily-interview-pro/reverse-words-in-a-string/test.py
@@ -0,0 +1,12 @@
+import unittest
+from solution import Solution
+
+
+class Test(unittest.TestCase):
+
+    def test_reverseWords(self):
+        self.assertEqual('ehT tac ni eht tah', Solution().reverseWords("The cat in the hat"))
+
+
+if __name__ == '__main__':
+    unittest.main()
