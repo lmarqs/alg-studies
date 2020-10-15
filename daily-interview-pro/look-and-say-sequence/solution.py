0eabda2281b4eb2139d45b5776ad3a1a1de9c172
lmarqs <lucas.paula@wipro.com>
2020-10-15 14:11:39 -0300

    add 'Look and Say Sequence'

---
+++ daily-interview-pro/look-and-say-sequence/solution.py
@@ -0,0 +1,22 @@
+def lookAndSayNth(n):
+    ans = '1'
+    for _ in range(1, n):
+        ans = lookAndSay(ans)
+    return ans
+
+
+def lookAndSay(n):
+    ans = ''
+    i = 0
+
+    while i < len(n):
+        k = n[i]
+        c = 1
+
+        while i + c < len(n) and n[i + c] == k:
+            c += 1
+
+        ans += str(c) + k
+        i += c
+
+    return ans
---
+++ daily-interview-pro/look-and-say-sequence/test.py
@@ -0,0 +1,23 @@
+import unittest
+from solution import lookAndSay, lookAndSayNth
+
+
+class Test(unittest.TestCase):
+    def test_lookAndSay(self):
+        self.assertEqual('', lookAndSay(''))
+        self.assertEqual('11', lookAndSay('1'))
+        self.assertEqual('21', lookAndSay('11'))
+        self.assertEqual('31', lookAndSay('111'))
+        self.assertEqual('111213', lookAndSay('123'))
+        self.assertEqual('31224311', lookAndSay('1112233331'))
+
+    def test_lookAndSayNth(self):
+        self.assertEqual('1', lookAndSayNth(1))
+        self.assertEqual('11', lookAndSayNth(2))
+        self.assertEqual('21', lookAndSayNth(3))
+        self.assertEqual('1211', lookAndSayNth(4))
+        self.assertEqual('111221', lookAndSayNth(5))
+
+
+if __name__ == '__main__':
+    unittest.main()
