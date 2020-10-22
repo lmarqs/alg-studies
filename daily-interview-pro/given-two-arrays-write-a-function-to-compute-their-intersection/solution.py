f43e60116fc5b75475cdbfc5952a6d1010e2a5a2
lmarqs <lucas.paula@wipro.com>
2020-10-22 19:53:44 -0300

    add solution for 'Given two arrays, write a function to compute their intersection'

--- daily-interview-pro/given-two-arrays-write-a-function-to-compute-their-intersection/solution.py
+++ daily-interview-pro/given-two-arrays-write-a-function-to-compute-their-intersection/solution.py
@@ -1,8 +1,12 @@
 class Solution:
-    def intersection(self, nums1, nums2):
+    # using a set of known numbers, time: O(N + M), space: O(2 min(N, M))
+    def intersection1(self, nums1, nums2):
         known = set()
         intersection = set()
         i = 0
+
+        if len(nums2) > len(nums1):
+            nums1, nums2 = nums2, nums1
 
         for n in nums1:
             while i < len(nums2) and not n in known:
@@ -11,5 +15,27 @@ class Solution:
 
             if n in known:
                 intersection.add(n)
+
+        return list(intersection)
+
+    # sorting the arrays, time: O(N log N + M log M), space: min(N, M)
+    def intersection2(self, nums1, nums2):
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        intersection = set()
+        i = 0
+        j = 0
+
+        while i < len(nums1) and j < len(nums2):
+            a = nums1[i]
+            b = nums2[j]
+
+            if nums1[i] == nums2[j]:
+                intersection.add(a)
+                i += 1
+                j += 1
+            elif a < b:
+                i += 1
+            else:
+                j += 1
 
         return list(intersection)
--- daily-interview-pro/given-two-arrays-write-a-function-to-compute-their-intersection/test.py
+++ daily-interview-pro/given-two-arrays-write-a-function-to-compute-their-intersection/test.py
@@ -5,7 +5,8 @@ from solution import Solution
 class Test(unittest.TestCase):
 
     def test_intersection(self):
-        self.assertEqual([9, 4], Solution().intersection([4, 9, 5], [9, 4, 9, 8, 4]))
+        self.assertEqual([9, 4], Solution().intersection1([4, 9, 5], [9, 4, 9, 8, 4]))
+        self.assertEqual([9, 4], Solution().intersection2([4, 9, 5], [9, 4, 9, 8, 4]))
 
 
 if __name__ == '__main__':
