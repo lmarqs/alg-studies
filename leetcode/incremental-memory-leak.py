91dfa77e13656c28d56c4ec26975da441fb1918c
lmarqs <lucas.marques@medprevonline.com>
2024-05-01 17:14:55 -0300

    [master] Add solution for 'Incremental Memory Leak' problem

---
+++ leetcode/incremental-memory-leak.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/incremental-memory-leak/description/
+
+import unittest
+
+class Solution:
+  def memLeak(self, memory1: int, memory2: int) -> list[int]:
+    i = 0
+
+    while True:
+      i += 1
+
+      if memory1 >= memory2:
+        memory1 -= i
+      else:
+        memory2 -= i
+
+      if memory1 < 0:
+        return [i, memory1 + i, memory2]
+
+      if memory2 < 0:
+        return [i, memory1, memory2 + i]
+
+param_list = [
+  [[3,1,0], 2, 2],
+  [[6,0,4], 8, 11],
+]
+
+class Test(unittest.TestCase):
+  def test_reversePrefix(self):
+    for expected, word, ch in param_list:
+      with self.subTest():
+        self.assertEqual(expected, Solution().memLeak(word, ch))
+
+if __name__ == '__main__':
+    unittest.main()
