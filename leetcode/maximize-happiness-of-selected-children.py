b22f22cd0d220e5153ddc796c0acd089c7e0fc7c
lmarqs <lucas.marques@medprevonline.com>
2024-05-09 08:50:04 -0300

    [master] feat: Add solution for 'Maximize Happiness of Selected Children' problem

---
+++ leetcode/maximize-happiness-of-selected-children.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/maximize-happiness-of-selected-children/description/
+
+import unittest
+
+
+class Solution:
+    def maximumHappinessSum(self, happiness, k):
+        happiness = sorted(happiness, reverse=True)
+
+        happiness_saved = 0
+
+        for i in range(k):
+            happiness_saved += max(happiness[i] - i, 0)
+
+        return happiness_saved
+
+
+param_list = [
+    [4, [1, 2, 3], 2],
+    [1, [1, 1, 1, 1], 2],
+    [5, [2, 3, 4, 5], 1],
+    [53, [12,1,42], 3]
+]
+
+
+class Test(unittest.TestCase):
+    def test_solution(self):
+        for expected, happiness, k in param_list:
+            with self.subTest():
+                self.assertEqual(expected, Solution().maximumHappinessSum(happiness, k))
+
+
+if __name__ == "__main__":
+    unittest.main()
