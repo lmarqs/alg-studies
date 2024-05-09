ceb710fee50ba1d1c183da959fb72e1759348414
lmarqs <lucas.marques@medprevonline.com>
2024-05-09 09:05:19 -0300

    [master] Refactor Fibonacci Number solution for better performance

--- leetcode/fibonacci-number.py
+++ leetcode/fibonacci-number.py
@@ -3,11 +3,35 @@
 import unittest
 
 class Solution(object):
-    def fib(self, n):
-        return 0
+    def __init__(self):
+        self.memo = {}
 
+    def fib(self, n):
+        if n in self.memo:
+            return self.memo[n]
+
+        if n == 0:
+            return 0
+
+        if n == 1:
+            return 1
+
+        self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
+
+        return self.memo[n]
 
 param_list = [
+    [0, 0],
+    [1, 1],
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [6765, 20],
+    [832040, 30],
+    [9227465, 35],
+    [24157817, 37],
+    [63245986, 39],
+    [280571172992510140037611932413038677189525, 200],
 ]
 
 
