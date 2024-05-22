afc934aa241ca89a965e6c772501cc80af7eee08
lmarqs <lucas.marques@medprevonline.com>
2024-05-22 09:19:41 -0300

    [master] feat: Add reverse integer function

---
+++ leetcode/reverse-integer.c
@@ -0,0 +1,32 @@
+int reverse(int x) {
+    if (x == -2147483648) {
+        return 0;
+    }
+
+    int y = 0;
+
+    unsigned short int isNegative = x < 0;
+
+    if (isNegative) {
+        x = -x;
+    }
+
+    while (x > 0) {
+        if (y > 214748364 && x > 7) {
+            return 0;
+        }
+
+        y *= 10;
+        y += x % 10;
+        x /= 10;
+    }
+
+    if (isNegative) {
+        y = -y;
+    }
+
+    return y;
+}
+
+int main() {
+}
