d8f821731c254c1c01b333c677988e1e8d5e1d76
Lucas Marques <marques.analista@gmail.com>
2025-07-10 20:19:33 -0300

    [master] refactor: Simplify reverse function by adjusting variable types and improving return logic

--- leetcode/reverse-integer.c
+++ leetcode/reverse-integer.c
@@ -5,13 +5,13 @@ int reverse(int32_t x) {
         return 0;
     }
 
-    int64_t y = 0;
-
     short int isNegative = x < 0;
 
     if (isNegative) {
         x = -x;
     }
+
+    uint64_t y = 0;
 
     while (x > 0) {
         y *= 10;
@@ -19,13 +19,9 @@ int reverse(int32_t x) {
         x /= 10;
     }
 
-    if (y >= 0b10000000000000000000000000000000) {
+    if (y >= 0b01111111111111111111111111111111) {
         return 0;
     }
 
-    if (isNegative) {
-        y = -y;
-    }
-
-    return y;
+    return isNegative ? -y : y;
 }
