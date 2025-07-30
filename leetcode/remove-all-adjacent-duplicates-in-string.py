db435f17aa03f6a14e02d281a9811d06e4fd0762
Lucas Marques <marques.analista@gmail.com>
2025-07-30 19:09:11 -0300

    [master] fix: Correct index handling in removeDuplicates method to prevent negative index

--- leetcode/remove-all-adjacent-duplicates-in-string.py
+++ leetcode/remove-all-adjacent-duplicates-in-string.py
@@ -10,7 +10,7 @@ class Solution:
         while i < len(s) - 1:
             if s[i] == s[i + 1]:
                 s = s[:i] + s[i + 2 :]
-                i -= 1
+                i = max(i - 1, 0)
             else:
                 i += 1
 
